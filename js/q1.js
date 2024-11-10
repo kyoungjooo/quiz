9e36f8cc6cc37ab42463d254903d6dfec226ac00
leekjoo <leekjoo1008@gmail.com>
2024-11-10 11:32:22 +0900

    문제풀이

--- js/q1.js
+++ js/q1.js
@@ -1,14 +1,35 @@
 /*
 변수 위치 찾기
+
 변수 a는 main 함수 안에서 사용된다
+//main 함수 안에 있는 지역변수
+
 변수 b는 dom api selector로 다른 이벤트에서 사용된다
+전역변수
+//const b = document.querySeletcor("...")
+
 변수 c는 메인 함수 안에서 실행되는 콜백함수 solution의 인자이다
+
 변수 d는 솔루션 함수에서 전달받은 c를 다른 형태로 바꿔준다.
+
 변수 e는 main함수의 최종 반환 값으로 향후 다른 함수에서 재사용된다.
 */
 
-function solution() {}
-function parse() {}
-function main() {
-  solution();
+//-----------
+
+const b = document.querySeletcor("...");
+
+//선언문
+function solution(c) {
+  const d = parse(c);
 }
+//선언문
+function parse() {}
+
+//선언문
+function main() {
+  let a, e;
+  solution(c); //실행문
+  return e;
+}
+//main()
--- js/q2.js
+++ js/q2.js
@@ -6,5 +6,16 @@
 ex) 길이기 80인 배열은 길이가 5로 분해한다면 16개의 배열을 요소로 갖는 배열을 반환한다
     ex) [1...80] => [[1...5], [6...10], [11...15], ... , [76...80]]
 */
-
-function division() {}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const num = 3;
+function division(arr) {
+  const result = arr.reduce((acc, cur, i) => {
+    if (i % num === 0) {
+      acc.push([]);
+    }
+    acc[acc.length - 1].push(cur);
+    return acc;
+  }, []);
+  console.log(result);
+}
+division(arr);
